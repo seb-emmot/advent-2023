31431da3cfc85ec821f7faa7b735635e1cdc2525
Sebastian Emmot <sebastian.emmot@gmail.com>
2023-12-08 20:57:15 +0100

    add day8 solutions

--- day8/day8.go
+++ day8/day8.go
@@ -24,18 +24,25 @@ func (n *Node) SetPaths(left, right *Node) {
 	}
 }
 
-func (n *Node) Print() {
-	fmt.Println(n.Value)
-	fmt.Println(n.Left)
-	fmt.Println(n.Right)
+func (n *Node) Print(prefix string) {
+	fmt.Println(prefix + n.Value)
+	if n.Value == "ZZZ" {
+		return
+	}
+	if n.Left != n {
+		n.Left.Print(prefix + " ")
+	}
+	if n.Right != n {
+		n.Right.Print(prefix + " ")
+	}
 }
 
 func Day8(input []string) {
 	moveSeq := input[0]
 
-	nodeMap := map[string]Node{}
+	nodeMap := map[string]*Node{}
 
-	fmt.Println(moveSeq)
+	// fmt.Println(moveSeq)
 
 	for _, row := range input[2:] {
 		parts := strings.Split(row, " = ")
@@ -45,7 +52,7 @@ func Day8(input []string) {
 		n, ok := nodeMap[value]
 
 		if !ok {
-			n = Node{Value: value}
+			n = &Node{Value: value}
 		}
 
 		lValue := nPairs[0]
@@ -54,31 +61,110 @@ func Day8(input []string) {
 		l, ok := nodeMap[lValue]
 
 		if !ok {
-			l = Node{Value: lValue}
+			l = &Node{Value: lValue}
 			nodeMap[lValue] = l
 		}
 
 		r, ok := nodeMap[rValue]
 
 		if !ok {
-			r = Node{Value: rValue}
+			r = &Node{Value: rValue}
 			nodeMap[rValue] = r
 		}
 
-		fmt.Println("setting", n, "l and r to", l, r)
-		n.SetPaths(&r, &l)
-
-		fmt.Println(n)
-
+		n.SetPaths(l, r)
 		nodeMap[value] = n
-
-		fmt.Println(row)
 	}
 
-	fmt.Println(nodeMap)
-	aNode := nodeMap["AAA"]
+	node := nodeMap["AAA"]
 
-	fmt.Println(aNode)
+	stepsP1 := 0
+	doneP1 := false
 
-	aNode.Print()
+	for !doneP1 {
+		for _, r := range moveSeq {
+			if !doneP1 {
+				if node.Value == "ZZZ" {
+					fmt.Println("DONE", "steps", stepsP1)
+					doneP1 = true
+				} else {
+					stepsP1++
+				}
+				if r == 'L' {
+					node = node.Left
+				} else if r == 'R' {
+					node = node.Right
+				} else {
+					panic("invalid move")
+				}
+			}
+		}
+	}
+	fmt.Println("P1", stepsP1)
+
+	p2nodes := []*Node{}
+
+	for k, v := range nodeMap {
+		if strings.HasSuffix(k, "A") {
+			p2nodes = append(p2nodes, v)
+		}
+	}
+
+	as := []int{}
+
+	for _, n := range p2nodes[:] {
+		cnt := 0
+		node := n
+
+		a := 0
+		b := 0
+		c := 0
+		for a == 0 || b == 0 || c == 0 {
+			for _, r := range moveSeq {
+				if r == 'L' {
+					node = node.Left
+				} else if r == 'R' {
+					node = node.Right
+				} else {
+					panic("invalid move")
+				}
+				cnt++
+				if strings.HasSuffix(node.Value, "Z") {
+					if a == 0 {
+						a = cnt
+					} else if b == 0 {
+						b = cnt
+					} else {
+						c = cnt
+						break
+					}
+					// fmt.Print(cnt, " ")
+				}
+			}
+
+			if node.Value == n.Value {
+				fmt.Println("break")
+				break
+			}
+		}
+		as = append(as, a)
+		fmt.Println(a, b-(a*2), c-(a*3))
+	}
+
+	result := as[0] * as[1] / GCD(as[0], as[1])
+
+	for i := 2; i < len(as); i++ {
+		result = result * as[i] / GCD(result, as[i])
+	}
+
+	fmt.Println("P2 res", result)
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
 }
--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	// input := utils.ReadFile("inputs/day8.txt")
-	input := utils.ReadFile("inputs/day8_sample.txt")
+	input := utils.ReadFile("inputs/day8.txt")
+	// input := utils.ReadFile("inputs/day8_sample.txt")
+	// input := utils.ReadFile("inputs/day8_sample2.txt")
+	// input := utils.ReadFile("inputs/day8_sampleb.txt")
 
 	day8.Day8(input)
 }
